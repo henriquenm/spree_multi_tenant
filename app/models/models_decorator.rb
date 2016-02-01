5b6f552a9f859365271f2eeb32b4a577f8166de6
henriquenm <marculo.henrique@gmail.com>
2016-02-01 12:19:24 -0200

    change son get base scope method

--- app/models/models_decorator.rb
+++ app/models/models_decorator.rb
@@ -33,11 +33,12 @@ end
 
 Spree::Core::Search::Base.class_eval do
     def get_base_scope
-      base_scope = Spree::Product.active
+      base_scope = Spree::Product.spree_base_scopes.active
       base_scope = base_scope.where(tenant_id: Multitenant.current_tenant.id) if Multitenant.current_tenant
       base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
       base_scope = get_products_conditions_for(base_scope, keywords)
       base_scope = add_search_scopes(base_scope)
+      base_scope = add_eagerload_scopes(base_scope)
       base_scope
     end
 end
